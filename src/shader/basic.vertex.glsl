a1937dcbd67dcd3470d02a0b4b1298f2b7593766
Tristen Horton <tristen@tristenhorton.com>
2018-11-24 13:13:52 -0700

    disengage low-precision shader for now

--- src/shader/basic.vertex.glsl
+++ src/shader/basic.vertex.glsl
@@ -12,10 +12,10 @@ out vec2 texcoord;
 void main()
 {
     vec4 position = model_view_projection * vec4(in_position, 1.0);
-    position.xyz = position.xyz / position.w;
+    /*position.xyz = position.xyz / position.w;
     position.x = floor(200.0 * position.x) / 200.0;
     position.y = floor(200.0 * position.y) / 200.0;
-    position.xyz *= position.w;
+    position.xyz *= position.w;*/
 
     float distance = length(model_view * position);
 
